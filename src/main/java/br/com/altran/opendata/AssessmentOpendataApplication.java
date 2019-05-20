8b92a4fef11debb75269a601a97ab9173e734dd1
eudes.justino <eudes.justino@accenture.com>
2019-05-20 09:01:58 -0300

    Creation of the base project.

---
+++ src/main/java/br/com/altran/opendata/AssessmentOpendataApplication.java
@@ -0,0 +1,13 @@
+package br.com.altran.opendata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentOpendataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentOpendataApplication.class, args);
+	}
+
+}
